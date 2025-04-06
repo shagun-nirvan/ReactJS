3e305b359c5cca7b86adf2e23afb26b8e0b69a0b
[Shagun Nirvan] <[nirvanshagun173@gmail.com]>
2025-04-06 11:57:59 +0530

    new

--- 01React/viteproject01/src/App.jsx
+++ 01React/viteproject01/src/App.jsx
@@ -1,7 +1,13 @@
+import Sha from "./Sha"
+
 function App(){
   return(
-    <h1>Shagun Nirvan</h1>
-  )
+    <>                  
+    <h1>Shagun Nirvan</h1>   
+    <Sha />
+    <p>new para</p> 
+    </>          //gives error as we can export only one element so we can use div tag
+   )                  // <> </> known as fragment used to wrap tags in one tag
 }
 
 export default App;
---
+++ 01React/viteproject01/src/Sha.jsx
@@ -0,0 +1,9 @@
+function Sha(){
+    return(
+        <h3>Shagun is ready</h3>
+
+    )
+    
+}
+
+export default Sha;
